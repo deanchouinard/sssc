87eb9502271c869f5bac143cc3dbfaba6be4071a
Dean Chouinard <deanchouinard@gmail.com>
2015-06-19 10:52:41 -0400

    update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # sssc
+An example taken from the Meteor discussion group topic [Sending a string from the server to the client](https://forums.meteor.com/t/sending-a-string-from-the-server-to-the-client/398). The code is taken from two answers provided by [rahul](https://forums.meteor.com/users/rahul/activity).
+
+The code illustrated some important Meteor concepts so I hooked it up to a UI to see how it worked.
+
+
+Had to remove autopublish.
+
+
--- sssc.js
+++ sssc.js
@@ -1,21 +1,7 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 
   Session.setDefault('mcString', "yp");
+
   Meteor.call("getMyString", function(err, res) {
     Session.set("mcString", res);
   });
@@ -40,9 +26,6 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
 
   Meteor.publish("myString", function() {
     // don't return a cursor from a database, just tell DDP to add your string to the publication and send that
